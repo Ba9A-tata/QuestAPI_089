f225292d79c49710a18fa9c34b456bb524aa546d
Ba9A-tata <yogadzakkii@gmail.com>
2025-12-18 12:20:00 +0700

    membuat class defaultcontainerapp

--- app/src/main/java/com/example/questapi_089/repositori/ContainerApp.kt
+++ app/src/main/java/com/example/questapi_089/repositori/ContainerApp.kt
@@ -1,5 +1,17 @@
 package com.example.questapi_089.repositori
 
+import okhttp3.logging.HttpLoggingInterceptor
+
 interface ContainerApp{
     val repositorySiswa: RepositoryDataSiswa
+}
+
+
+class DefaultContainerApp : ContainerApp{
+    private val baseurl = "http://10.0.2.2/umyTI/"
+
+    val logging = HttpLoggingInterceptor().apply {
+        level= HttpLoggingInterceptor.Level.BODY
+
+    }
 }
