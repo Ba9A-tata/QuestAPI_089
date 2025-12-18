ebcceea5f4594c43c466c3e886049d4765d48ee8
Ba9A-tata <yogadzakkii@gmail.com>
2025-12-18 19:04:30 +0700

    menambahkan clas AplikasiDataSsiswa

--- app/src/main/java/com/example/questapi_089/repositori/ContainerApp.kt
+++ app/src/main/java/com/example/questapi_089/repositori/ContainerApp.kt
@@ -9,16 +9,17 @@ import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 
-interface ContainerApp{
+interface ContainerApp {
+    // Sediakan dua nama agar tidak error jika ViewModel memanggil salah satunya
     val repositoryDataSiswa: RepositoryDataSiswa
+    val repositorySiswa: RepositoryDataSiswa
 }
 
-class DefaultContainerApp : ContainerApp{
+class DefaultContainerApp : ContainerApp {
     private val baseurl = "http://10.0.2.2/tiumy/"
 
     val logging = HttpLoggingInterceptor().apply {
-        level= HttpLoggingInterceptor.Level.BODY
-
+        level = HttpLoggingInterceptor.Level.BODY
     }
 
     val klien = OkHttpClient.Builder()
@@ -41,12 +42,19 @@ class DefaultContainerApp : ContainerApp{
         retrofit.create(ServiceApiSiswa::class.java)
     }
 
+    // Implementasikan keduanya ke sumber yang sama
     override val repositoryDataSiswa: RepositoryDataSiswa by lazy {
-        JaringanRepositoryDataSiswa(retrofitService) }
+        JaringanRepositoryDataSiswa(retrofitService)
+    }
+
+    override val repositorySiswa: RepositoryDataSiswa by lazy {
+        repositoryDataSiswa
+    }
 }
 
 class AplikasiDataSiswa : Application() {
     lateinit var containerApp: ContainerApp
+
     override fun onCreate() {
         super.onCreate()
         containerApp = DefaultContainerApp()
