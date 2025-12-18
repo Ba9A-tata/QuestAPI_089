75dff70d2915a58fef8f6c4c85f0a32398074f67
Ba9A-tata <yogadzakkii@gmail.com>
2025-12-18 18:05:53 +0700

    menambahkan object DestinasiHome

---
+++ app/src/main/java/com/example/questapi_089/uicontroller/route/DestinasiHome.kt
@@ -0,0 +1,8 @@
+package com.example.questapi_089.uicontroller.route
+
+import com.example.localrestapi.R
+
+object DestinasiHome : DestinasiNavigasi {
+    override val route = "home"
+    override val titleRes = R.string.app_name
+}
