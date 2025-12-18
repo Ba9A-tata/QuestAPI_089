c2867a8a3d728ee3622a704cbe9d79073bfae15c
Ba9A-tata <yogadzakkii@gmail.com>
2025-12-18 12:05:32 +0700

    membuat method POST

--- app/src/main/java/com/example/questapi_089/apiservice/ServiceApiSiswa.kt
+++ app/src/main/java/com/example/questapi_089/apiservice/ServiceApiSiswa.kt
@@ -1,5 +1,7 @@
 package com.example.questapi_089.apiservice
 
+import com.example.questapi_089.modeldata.DataSiswa
+import retrofit2.http.Body
 import retrofit2.http.GET
 import retrofit2.http.POST
 
@@ -8,5 +10,6 @@ interface  ServiceApiSiswa {
     @GET("bacaTeman.php")
     suspend fun getSiswa() ; List<DataSiswa>
 
-
+    @POST("insertTM.php")
+    suspend fun  postSiswa(@Body dataSiswa: DataSiswa):retrofit2.Response<Void>
 }
