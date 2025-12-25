8608392049b3159b73882fb766559cb79a69cb97
Ba9A-tata <yogadzakkii@gmail.com>
2025-12-25 13:12:50 +0700

    menambahkan seald interface statusuidetial

---
+++ app/src/main/java/com/example/questapi_089/viewmodel/DetailViewModel.kt
@@ -0,0 +1,15 @@
+package com.example.questapi_089.viewmodel
+
+import android.annotation.SuppressLint
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import com.example.questapi_089.modeldata.DataSiswa
+import com.example.questapi_089.repositori.RepositoryDataSiswa
+import retrofit2.HttpException
+import retrofit2.Response
+import java.io.IOException
+
+sealed interface StatusUIDetail {
