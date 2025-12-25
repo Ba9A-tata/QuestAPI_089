b2d926a69c22f2e6b52dd40bf4cd8f957feda6d9
Ba9A-tata <yogadzakkii@gmail.com>
2025-12-25 13:58:22 +0700

    menambahkan suspen fun getsatusiswa

--- app/src/main/java/com/example/questapi_089/apiservice/ServiceApiSiswa.kt
+++ app/src/main/java/com/example/questapi_089/apiservice/ServiceApiSiswa.kt
@@ -4,6 +4,7 @@ import com.example.questapi_089.modeldata.DataSiswa
 import retrofit2.http.Body
 import retrofit2.http.GET
 import retrofit2.http.POST
+import retrofit2.http.Query
 
 interface  ServiceApiSiswa {
 
@@ -12,4 +13,9 @@ interface  ServiceApiSiswa {
 
     @POST("insertTM.php")
     suspend fun  postSiswa(@Body dataSiswa: DataSiswa):retrofit2.Response<Void>
+
+    @GET("baca1Teman.php/{id}")
+    suspend fun getSatuSiswa(@Query("id") id: Int): DataSiswa
+
+
 }
