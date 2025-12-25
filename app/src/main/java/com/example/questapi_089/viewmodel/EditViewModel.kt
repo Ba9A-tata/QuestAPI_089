146cfb482b4bf2b5e86633d95c286a1079ec8764
Ba9A-tata <yogadzakkii@gmail.com>
2025-12-25 13:20:38 +0700

    menambahkan menambahkan class edit viewmodel

---
+++ app/src/main/java/com/example/questapi_089/viewmodel/EditViewModel.kt
@@ -0,0 +1,19 @@
+package com.example.questapi_089.viewmodel
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.questapi_089.modeldata.DetailSiswa
+import com.example.questapi_089.modeldata.UIStateSiswa
+import com.example.questapi_089.modeldata.toDataSiswa
+import com.example.questapi_089.repositori.RepositoryDataSiswa
+import kotlinx.coroutines.launch
+import retrofit2.Response
+
+class EditViewModel(savedStateHandle: SavedStateHandle, private val repositoryDataSiswa:
+RepositoryDataSiswa
+): ViewModel() {
+    
