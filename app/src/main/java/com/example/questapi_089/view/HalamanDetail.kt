d0499d848324c9183e58a53945a97fd17aa4bb61
Ba9A-tata <yogadzakkii@gmail.com>
2025-12-25 12:42:39 +0700

    menambahkan scaflod siswatopappbar

--- app/src/main/java/com/example/questapi_089/view/HalamanDetail.kt
+++ app/src/main/java/com/example/questapi_089/view/HalamanDetail.kt
@@ -1,6 +1,9 @@
 package com.example.questapi_089.view
 
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import com.example.questapi_089.viewmodel.provider.PenyediaViewModel
 
 @Composable
 fun DetailSiswaScreen(
@@ -9,5 +12,13 @@ fun DetailSiswaScreen(
     modifier: Modifier = Modifier,
     viewModel: DetailViewModel = viewModel(factory = PenyediaViewModel.Factory)
 ) {
+    Scaffold(
+        topBar = {
+            SiswaTopAppBar(
+                title = stringResource(DestinasiDetail.titleRes),
+                canNavigateBack = true,
+                navigateUp = navigateBack
+            )
+        },
 
 }
