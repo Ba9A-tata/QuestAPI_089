cbce95ec14d3b28ff227e7556d8e4d786f86b356
Ba9A-tata <yogadzakkii@gmail.com>
2025-12-25 14:11:58 +0700

    menambahkan val routewithargs

--- app/src/main/java/com/example/questapi_089/uicontroller/route/DestinasiDetail.kt
+++ app/src/main/java/com/example/questapi_089/uicontroller/route/DestinasiDetail.kt
@@ -1,8 +1,10 @@
 package com.example.questapi_089.uicontroller.route
 
+import com.example.localrestapi.R
+
 object DestinasiDetail : DestinasiNavigasi {
     override val route = "detail_siswa"
     override val titleRes = R.string.detail_siswa
     const val itemIdArg = "idSiswa"
-
+    val routeWithArgs = "$route/{$itemIdArg}"
 }
