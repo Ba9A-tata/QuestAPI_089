7bc6074faac81bb60f6ac8b4342e063390db2b74
Ba9A-tata <yogadzakkii@gmail.com>
2025-12-25 12:54:29 +0700

    menambahkan material card di dlam fungsi detaildatasiswa

--- app/src/main/java/com/example/questapi_089/view/HalamanDetail.kt
+++ app/src/main/java/com/example/questapi_089/view/HalamanDetail.kt
@@ -11,7 +11,6 @@ import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Edit
-import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -130,4 +129,10 @@ private fun BodyDetailDataSiswa(
 @Composable
 fun DetailDataSiswa(
     siswa: DataSiswa, modifier: Modifier = Modifier
-)
+) {
+    Card(
+        modifier = modifier, colors = CardDefaults.cardColors(
+            containerColor = MaterialTheme.colorScheme.primaryContainer,
+            contentColor = MaterialTheme.colorScheme.onPrimaryContainer
+        )
+    )
