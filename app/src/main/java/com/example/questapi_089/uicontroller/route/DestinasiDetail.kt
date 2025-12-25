8113a4e8be3e2117974690a633333ee1ec4b6da6
Ba9A-tata <yogadzakkii@gmail.com>
2025-12-25 14:10:43 +0700

    menambahkan ovveride val titleres

--- app/src/main/java/com/example/questapi_089/uicontroller/route/DestinasiDetail.kt
+++ app/src/main/java/com/example/questapi_089/uicontroller/route/DestinasiDetail.kt
@@ -2,5 +2,6 @@ package com.example.questapi_089.uicontroller.route
 
 object DestinasiDetail : DestinasiNavigasi {
     override val route = "detail_siswa"
+    override val titleRes = R.string.detail_siswa
 
 }
