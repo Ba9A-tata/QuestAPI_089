8223e377a7fd7b4ae3f035b647238370b4f0dd33
Ba9A-tata <yogadzakkii@gmail.com>
2025-12-25 13:10:11 +0700

    menambahkan fun editsiswascreen

---
+++ app/src/main/java/com/example/questapi_089/view/HalamanEdit.kt
@@ -0,0 +1,19 @@
+package com.example.questapi_089.view
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.questapi_089.viewmodel.provider.PenyediaViewModel
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun EditSiswaScreen(
+    navigateBack: () -> Unit,
+    onNavigateUp: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: EditViewModel = viewModel(factory = PenyediaViewModel.Factory)
+)
