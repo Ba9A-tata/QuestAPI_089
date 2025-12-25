fcab796033505a0b13b9b1e071056977477d0770
Ba9A-tata <yogadzakkii@gmail.com>
2025-12-25 14:11:21 +0700

    menambahkan const val item

--- app/src/main/java/com/example/questapi_089/uicontroller/route/DestinasiDetail.kt
+++ app/src/main/java/com/example/questapi_089/uicontroller/route/DestinasiDetail.kt
@@ -3,5 +3,6 @@ package com.example.questapi_089.uicontroller.route
 object DestinasiDetail : DestinasiNavigasi {
     override val route = "detail_siswa"
     override val titleRes = R.string.detail_siswa
+    const val itemIdArg = "idSiswa"
 
 }
